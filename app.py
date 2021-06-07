28958ae1d6d05632550e95e7587ce185b2572329
SHUBHAM BANSAL <35103998+SHUBHAMBANSAL001@users.noreply.github.com>
2021-06-07 06:50:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import os, sys
+from flask import Flask
+import views
+
+app = Flask(__name__)
+app.add_url_rule("/", view_func = views.root, methods=['GET'])
+app.add_url_rule("/read_ocr", view_func = views.read_ocr, methods=['POST'])
+print("Server Ready", flush=True)
+
+if __name__ == "__main__":    
+  app.run(host='0.0.0.0', port=4000)
